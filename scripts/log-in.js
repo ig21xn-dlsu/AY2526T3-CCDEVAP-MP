fb09654ee16b0a0ec1a0b2e45a5e79bf1008691d
ig21xn-dlsu <ian_gabriel_ilagan@dlsu.edu.ph>
2026-06-16 00:41:51 +0800

    asdasdadas

--- scripts/log-in.js
+++ scripts/log-in.js
@@ -6,3 +6,24 @@ function showPassword() {
         x.type = "password";
     }
 }
+
+function changeImage() {
+    let x = document.getElementById("login_image");
+
+    x.classList.add("hidden");
+    
+    setTimeout(function() {
+        
+        if (x.getAttribute('src') == "/frontend/images/dorm_1.jpg") {
+            x.src = "/frontend/images/dorm_2.jpg";
+        } else if (x.getAttribute('src') == "/frontend/images/dorm_2.jpg") {
+            x.src = "/frontend/images/dorm_3.jpg";
+        } else {
+            x.src = "/frontend/images/dorm_1.jpg";
+        }
+        x.classList.remove("hidden");
+        
+    }, 500);
+}
+
+setInterval(changeImage, 10000);

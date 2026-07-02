edcda8daa05e70d99076a086ee10c2dbb30f4b1c
ig21xn-dlsu <ian_gabriel_ilagan@dlsu.edu.ph>
2026-07-02 16:32:09 +0800

    AHHH GUMAGANA LOGIN VIA POSTMAN YESSSSSSS

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,11 @@
 const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 const validator = require('validator');
+const jwt = require('jsonwebtoken');
+
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: '3d' }); // expires in three days
+}
 
 // This function handles "register a new user"
 const registerUser = async (req, res) => {
@@ -17,7 +22,7 @@ const registerUser = async (req, res) => {
     }
 
     if (!validator.isStrongPassword(password)) {
-      return res.status(400).json({ message: 'Password not strong enough' }); 
+      return res.status(400).json({ message: 'Password should start hitting the gym, eh?' }); 
     }
 
     if (!(password === confirmPassword)) {
@@ -53,21 +58,27 @@ const registerUser = async (req, res) => {
 // This function handles "logging-in"
 const loginUser = async (req, res) => {
   try {
-    const { email, password} = req.body; 
+    const { email, password } = req.body;
+    
+    if (!email || !password) {
+      return res.status(400).json({ message: 'All fields are required.' });
+    }
+
     const existingUser = await User.findOne({ email });
 
     if (!existingUser) {
       return res.status(400).json({ message: 'User does not exists' });
+    } 
+      
+    const match = await bcrypt.compare(password, existingUser.password);
+      
+    if (!match) {
+      return res.status(400).json({ message: 'Incorrect Password' });
     } else {
-      const match = await bcrypt.compare(password, existingUser.password);
-      if (!match) {
-        return res.status(400).json({ message: 'Incorrect Password' });
-      } else {
-        return res.status(201).json({ message: 'Logged In!' });
-      }
+      const token = createToken(existingUser._id);
+      return res.status(201).json({ message: 'Logged In!', token });
     }
-
-
+    
   } catch (err) {
     console.error(err.message);
     res.status(500).json({ message: 'Server error' });

192e7029826adc4721965a47da520489e11f3492
ig21xn-dlsu <ian_gabriel_ilagan@dlsu.edu.ph>
2026-07-03 19:16:27 +0800

    Tryna fix some css problems

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -20,7 +20,7 @@ function Login() {
             }),
         });
 
-      const data = await response.json();w
+      const data = await response.json();
 
         if (!response.ok) {
             console.error('LogIn Failed:', data.message);

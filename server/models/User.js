8e0ec8825f4c7b2a90844779e2e682874110890d
ig21xn-dlsu <ian_gabriel_ilagan@dlsu.edu.ph>
2026-07-04 14:58:19 +0800

    Added admin role for the database

--- server/models/User.js
+++ server/models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
     lastName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, enum: ['student', 'manager'], required: true },
+    role: { type: String, enum: ['student', 'manager', 'admin'], required: true },
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);

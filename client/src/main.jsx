ef16308efb469d752346c0564fa2384e9d4cd394
ig21xn-dlsu <ian_gabriel_ilagan@dlsu.edu.ph>
2026-07-06 08:52:18 +0800

    Moved index.css into the stylesheets folder

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './stylesheets/index.css'
 import App from './App.jsx'
 import { AuthContextProvider } from './context/AuthContext.jsx'
 

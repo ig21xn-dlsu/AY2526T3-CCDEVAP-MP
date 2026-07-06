6bd8355b844efbed2c934d28d666a984562075ca
PeelyMart <philipmartinantolihao@gmail.com>
2026-07-07 02:16:00 +0800

    /uploads as a static directory

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const connectDB = require('./config/db');
 const mongoose = require('mongoose')
 const authRoutes = require('./routes/authRoutes');
-
+const path = require("path");
 connectDB();
 
 const app = express();
@@ -18,6 +18,6 @@ app.use(cors({
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
-
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

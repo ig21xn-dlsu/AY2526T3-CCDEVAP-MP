9964c5a087565d8272320661b906546fe1ef22ed
cikat1005 <asia_teo@dlsu.edu.ph>
2026-07-15 21:36:01 +0800

    addedd credentials: true or else it cant fetch on discoverypage

--- server/server.js
+++ server/server.js
@@ -28,7 +28,8 @@ const app = express();
 app.use(cors({
   origin: 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
 }));
 
 
